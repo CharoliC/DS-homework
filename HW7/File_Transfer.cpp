73684710b3d88703f6a7850277560e77c3f8d80a
CharoliC <CharoliC@gmail.com>
2023-01-24 17:57:38 +0800

    1/24 commit

---
+++ HW7/File_Transfer.cpp
@@ -0,0 +1,73 @@
+#include <cstdio>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int *UFset;
+
+void Init(UFset &S);
+void Union(UFset &S, int rt1, int rt2);
+int Find(UFset &S, int x);
+
+int main() {
+    int n, k;
+    int c1, c2;
+    char op;
+
+    scanf("%d", &n);
+
+    //s[0] reserved (because computers numbered from 1 to N)
+    UFset s = (int*)malloc((n + 1) * sizeof(int));
+    memset(s, -1, (n + 1) * sizeof(int));
+
+    while((op = getchar()) != 'S') {
+        switch(op) {
+            case 'I':
+                scanf("%d%d", &c1, &c2);
+                Union(s, Find(s, c1), Find(s, c2));
+                break;
+
+            case 'C':
+                scanf("%d%d", &c1, &c2);
+                if(Find(s, c1) != Find(s, c2)) {
+                    printf("no\n");
+                }
+                else {
+                    printf("yes\n");
+                }
+                break;
+        }
+    }
+
+    //count components
+    k = 0;
+    for(int i=1; i<=n; i++) {
+        if(s[i] < 0) {
+            k++;
+        }
+    }
+
+    if(k == 1) {
+        printf("The network is connected.");
+    }
+    else {
+        printf("There are %d components.", k);
+    }
+
+    return 0;
+}
+
+void Union(UFset &S, int rt1, int rt2) {
+    if(rt1 != rt2) {
+        //printf("union %d and %d\n", rt1, rt2);
+        S[rt2] = rt1;
+    }
+}
+
+int Find(UFset &S, int x) {
+    if(S[x] < 0) {
+        return x;
+    }
+    else {
+        return S[x] = Find(S, S[x]);
+    }
+}
--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 ZJU数据结构基础PTA作业（复习重写，没交OJ）
 
 
-| 目录  |           题目           |                    内容                    |
-| :---: | :----------------------: | :----------------------------------------: |
-|  HW2  |   Add Two Polynomials    |             两个链表多项式相加             |
-|  HW2  |   Reverse Linked List    |                 单链表反转                 |
-|  HW3  |       Pop Sequence       |             栈能否弹出特定序列             |
-|  HW4  |        Isomorphic        |             两个二叉树是否同构             |
-|  HW4  |   ZigZagging on a Tree   |               之字遍历二叉树               |
-| bonus | One Way In, Two Ways Out |     输入受限的双端队列能否弹出特定序列     |
-| labs  |     Tree Traversals      | 用不完整的前中后序构造二叉树，然后层序遍历 |
+| 目录  |           题目           |                     内容                     |
+| :---: | :----------------------: | :------------------------------------------: |
+|  HW2  |   Add Two Polynomials    |              两个链表多项式相加              |
+|  HW2  |   Reverse Linked List    |                  单链表反转                  |
+|  HW3  |       Pop Sequence       |              栈能否弹出特定序列              |
+|  HW4  |        Isomorphic        |              两个二叉树是否同构              |
+|  HW4  |   ZigZagging on a Tree   |                之字遍历二叉树                |
+|  HW7  |      File Transfer       | 构造并查集，进行合并和查找，最后数出集合数目 |
+| bonus | One Way In, Two Ways Out |      输入受限的双端队列能否弹出特定序列      |
+| labs  |     Tree Traversals      |  用不完整的前中后序构造二叉树，然后层序遍历  |
